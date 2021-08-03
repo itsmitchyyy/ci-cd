acfff8a8e3cdadfb4791c92b55332ec72917947c
Mitch Alforque <mitch@vananaz.com>
2021-08-03 15:46:34 +0800

    register page

--- src/navigators/root/RootNavigator.tsx
+++ src/navigators/root/RootNavigator.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { LoginPage } from '../../components/pages/LoginPage';
+import { RegisterPage } from '../../components/pages/RegisterPage';
 import { ResetPasswordPage } from '../../components/pages/ResetPasswordPage';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
@@ -11,6 +12,7 @@ export default function RootNavigator() {
         <Suspense fallback={<h1>Loading...</h1>}>
           <Route path="/" exact component={LoginPage} />
           <Route path="/reset-password" exact component={ResetPasswordPage} />
+          <Route path="/register" exact component={RegisterPage} />
         </Suspense>
       </Switch>
     </BrowserRouter>
